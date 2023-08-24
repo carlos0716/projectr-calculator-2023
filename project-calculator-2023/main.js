aa2eef0a47ba673de75b8edf1aaf14053b044924
carlos0716 <chulezyc@gmail.com>
2023-08-23 21:08:43 -0600

    changes the value of accumulator in the equal function to result this allows to continuing the next operation with this result

--- project-calculator-2023/main.js
+++ project-calculator-2023/main.js
@@ -45,37 +45,19 @@ function operation(operandA, operator, operandB) {
 }
 
 const allNumberBtns = document.querySelectorAll('.number');
-const sevenNumber = document.getElementById('seven');
-const eightNumber = document.getElementById('eight');
 const sumButton = document.getElementById('sum');
+const restButton = document.getElementById('rest');
 const equalButton = document.getElementById('equal')
 
-// sevenNumber.addEventListener('click', e => {
-//         const value = e.target.value;
-//         operandA = Number(output.innerHTML += value);
-// });
-    
 sumButton.addEventListener('click', e => {
     operator = '+';
     operandA = accumulator;
     accumulator = 0;
-    console.log(operandA);
+    //console.log(operandA);
     output.innerHTML = '';
-
-    
 });
 
-// eightNumber.addEventListener('click', e => {
-//         const value = e.target.value;
-//         operandB = Number(output.innerHTML += value);
-// });
-
 equalButton.addEventListener('click', equalTo);
-
-// function myFunctionB(e) {
-//     const value = e.target.value;
-//     operandB = output.innerHTML += value;
-// }
 
 const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', e => {
@@ -87,32 +69,14 @@ clearButton.addEventListener('click', e => {
  
 const output = document.querySelector('#output');
 
-// function test() {
-//     //console.log('crap')
-//     let result = operation(operandA, operator, operandB);
-//     console.log(result);
-//     operandA = result;
-//     return output.innerHTML = result;
-// }
-
 function equalTo() {
-    console.log(operandA);
-    console.log(accumulator);
-    
     operandB = accumulator;
     const result = operation(operandA, operator, operandB);
     output.innerHTML = result;
+    operandA = 0;
     operandB = 0;
-    return operandA = result;
+    accumulator = result;
 }
-
-
-// function pressSum() {
-//     sumButton.addEventListener('click', e => {
-//         operator = '+';
-//         output.innerHTML='';
-//     })
-// }
 
 allNumberBtns.forEach(element => {
     element.addEventListener('click', myFunction);
@@ -123,5 +87,9 @@ function myFunction(e) {
     const result= Number(output.innerHTML += value);
     accumulator = result;
     
-    console.log(accumulator);
+    //console.log(accumulator);
 };
+
+console.log(operandA);
+console.log(operandB);
+console.log(accumulator);
