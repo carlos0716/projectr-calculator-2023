8a543a57461b87f17030fa6ef7d728dfef8d627c
carlos0716 <chulezyc@gmail.com>
2023-08-23 20:50:10 -0600

    function to test the sum and equal buttons in an operation added

--- project-calculator-2023/main.js
+++ project-calculator-2023/main.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    let result = (a + b).toFixed(4);
+    let result = (a + b);
     return Number(result)
 }
 
@@ -26,6 +26,7 @@ function divide(a, b) {
 let operandA;
 let operandB;
 let operator;
+let accumulator;
 
 function operation(operandA, operator, operandB) {
  if (operator === '+') {
@@ -43,4 +44,84 @@ function operation(operandA, operator, operandB) {
  }
 }
 
+const allNumberBtns = document.querySelectorAll('.number');
+const sevenNumber = document.getElementById('seven');
+const eightNumber = document.getElementById('eight');
+const sumButton = document.getElementById('sum');
+const equalButton = document.getElementById('equal')
+
+// sevenNumber.addEventListener('click', e => {
+//         const value = e.target.value;
+//         operandA = Number(output.innerHTML += value);
+// });
+    
+sumButton.addEventListener('click', e => {
+    operator = '+';
+    operandA = accumulator;
+    accumulator = 0;
+    console.log(operandA);
+    output.innerHTML = '';
+
+    
+});
+
+// eightNumber.addEventListener('click', e => {
+//         const value = e.target.value;
+//         operandB = Number(output.innerHTML += value);
+// });
+
+equalButton.addEventListener('click', equalTo);
+
+// function myFunctionB(e) {
+//     const value = e.target.value;
+//     operandB = output.innerHTML += value;
+// }
+
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', e => {
+    output.innerHTML = '';
+    operandA = 0;
+    operandB = 0;
+    operator = '';
+});
  
+const output = document.querySelector('#output');
+
+// function test() {
+//     //console.log('crap')
+//     let result = operation(operandA, operator, operandB);
+//     console.log(result);
+//     operandA = result;
+//     return output.innerHTML = result;
+// }
+
+function equalTo() {
+    console.log(operandA);
+    console.log(accumulator);
+    
+    operandB = accumulator;
+    const result = operation(operandA, operator, operandB);
+    output.innerHTML = result;
+    operandB = 0;
+    return operandA = result;
+}
+
+
+// function pressSum() {
+//     sumButton.addEventListener('click', e => {
+//         operator = '+';
+//         output.innerHTML='';
+//     })
+// }
+
+allNumberBtns.forEach(element => {
+    element.addEventListener('click', myFunction);
+});
+
+function myFunction(e) {
+    const value = e.target.value;
+    const result= Number(output.innerHTML += value);
+    accumulator = result;
+    
+    console.log(accumulator);
+};
